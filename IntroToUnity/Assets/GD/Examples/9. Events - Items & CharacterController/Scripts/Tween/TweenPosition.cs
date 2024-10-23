3931fba4d7607e5ac8146d35c7d1efd8e7d9eca5
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-10-23 10:05:44 +0100

    :sparkles: Added TweenPosition

--- IntroToUnity/Assets/GD/Examples/9. Events - Items & CharacterController/Scripts/Tween/TweenPosition.cs
+++ IntroToUnity/Assets/GD/Examples/9. Events - Items & CharacterController/Scripts/Tween/TweenPosition.cs
@@ -1,34 +1,68 @@
 using DG.Tweening;
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
+/// <summary>
+/// Uses DoTween to tween the position of the object.
+/// </summary>
 public class TweenPosition : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("The amount to move the object by")]
-    private Vector3 moveDelta;
+    private Vector3 positionDelta = Vector3.up;
 
     [SerializeField]
     [Range(0.1f, 10f)]
+    [Tooltip("The duration of the tween in seconds")]
     private float durationSecs = 0.1f;
 
     [SerializeField]
+    [Tooltip("The ease function to use for the tween")]
     private Ease easeFunction = Ease.Linear;
 
+    [SerializeField]
+    [Range(-1, 100)]
+    private int loopCount = 1;
+
+    [SerializeField]
+    [Tooltip("The type of loop to use for the tween")]
+    private LoopType loopType = LoopType.Restart;
+
+    [SerializeField]
+    [Tooltip("Event to call when the tween has completed")]
+    private UnityEvent onComplete;
+
     private Vector3 originalPosition;
-    private Vector3 originalScale;
 
     // Start is called before the first frame update
     private void Start()
     {
+        //Save the original position of the object
         originalPosition = transform.position;
-        originalScale = transform.localScale;
 
-        //transform.DOMove(originalPosition + moveDelta, durationSecs)
-        //   .SetEase(easeFunction);
+        //Start the tween
+        StartTween();
+    }
 
-        transform.DOScale(originalScale * 1.5f, durationSecs)
+    /// <summary>
+    /// Called to start the tween.
+    /// </summary>
+    public void StartTween()
+    {
+        //Move the object by the positionDelta over the durationSecs using the easeFunction and the loopCount and loopType
+        transform.DOMove(originalPosition + positionDelta, durationSecs)
            .SetEase(easeFunction)
-           .SetLoops(3, LoopType.Yoyo)
-           .OnComplete(() => Debug.Log("Tween Complete"));
+           .SetLoops(loopCount, loopType)
+           .OnComplete(OnComplete);
+    }
+
+    /// <summary>
+    /// Called when the tween has completed.
+    /// </summary>
+    protected void OnComplete()
+    {
+        //Notify listeners that the tween has completed
+        onComplete?.Invoke();
     }
 }
